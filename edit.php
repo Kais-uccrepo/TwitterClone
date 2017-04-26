94fcfe3c9e3334d91d147bbf2fc0728d10353dbf
kaisjessa <kais.jessa@ucc.on.ca>
2017-04-26 14:36:07 -0400

    search

--- edit.php
+++ edit.php
@@ -1,23 +1,34 @@
 <html>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<body>
-    
+		Submit
+<form class="navbar-form navbar-left" role="search">
+  <div class="form-group">
+    <input type="text" class="form-control" placeholder="Search">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+<br />
+<br />
+<br />
+
 	<?php
-	
+
 	    // pass in some info;
-		require("common.php"); 
-		
-		if(empty($_SESSION['user'])) { 
-  
-			// If they are not, we redirect them to the login page. 
+		require("common.php");
+
+		if(empty($_SESSION['user'])) {
+
+			// If they are not, we redirect them to the login page.
 			$location = "http://" . $_SERVER['HTTP_HOST'] . "/login.php";
 			echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
 			//exit;
-         
-        	// Remember that this die statement is absolutely critical.  Without it, 
-        	// people can view your members-only content without logging in. 
-        	die("Redirecting to login.php"); 
-    	} 
-		
+
+        	// Remember that this die statement is absolutely critical.  Without it,
+        	// people can view your members-only content without logging in.
+        	die("Redirecting to login.php");
+    	}
+
 		// To access $_SESSION['user'] values put in an array, show user his username
 		$arr = array_values($_SESSION['user']);
 		$user = $arr[1];
@@ -31,7 +42,7 @@
 
 		// create query
 		$query = "SELECT * FROM symbols";
-       
+
 		// execute query
 		$result = mysqli_query($connection,$query) or die ("Error in query: $query. ".mysql_error());
 
@@ -51,7 +62,7 @@
 		    echo "</table>";
 
 		} else {
-			
+
     		// print status message
     		echo "No rows found!";
 		}
@@ -62,7 +73,7 @@
 		// set variable values to HTML form inputs
 		$country = $_POST['country'];
     	$animal = $_POST['animal'];
-		
+
 		// check to see if user has entered anything
 		if ($animal != "") {
 	 		// build SQL query
@@ -72,7 +83,7 @@
 			// refresh the page to show new update
 	 		echo "<meta http-equiv='refresh' content='0'>";
 		}
-		
+
 		// if DELETE pressed, set an id, if id is set then delete it from DB
 		if (isset($_GET['id'])) {
 
@@ -82,25 +93,27 @@
 
 			// run the query
      		$result = mysqli_query($connection,$query) or die ("Error in query: $query. ".mysql_error());
-			
+
 			// reset the url to remove id $_GET variable
 			$location = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
 			echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
 			exit;
-			
+
 		}
-		
+
 		// close connection
 		mysqli_close($connection);
 
 	?>
-    
+
+		<br />
+		<br />
     <!-- This is the HTML form that appears in the browser -->
    	<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
     	Roast: <input type="text" name="animal">
     	<input type="submit" name="submit">
     </form>
     <form action="logout.php" method="post"><button>Log out</button></form>
-    
+
 	</body>
 </html>
